d36dc430098f3159629959d2f4339064a219f993
GabrielVodopivec <vodopivecgabriel@gmail.com>
2023-09-25 03:39:52 -0300

    first commit

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,23 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Product } from './products';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+import { Product } from "./products";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: "root",
 })
 export class CartService {
-  items: Product[] = [];
-  constructor() {}
+    items: Product[] = [];
+    constructor(private httpClient: HttpClient) {}
 
-  addToCart(product: Product) {
-    this.items.push(product);
-  }
+    addToCart(product: Product) {
+        this.items.push(product);
+    }
 
-  getItems() {
-    return this.items;
-  }
+    getItems() {
+        return this.items;
+    }
 
-  clearCart() {
-    this.items = [];
-    return this.items;
-  }
+    clearCart() {
+        this.items = [];
+        return this.items;
+    }
+
+    getShippingPrices() {
+        return this.httpClient.get<{ type: string; price: number }[]>(
+            "/assets/shipping.json"
+        );
+    }
 }
